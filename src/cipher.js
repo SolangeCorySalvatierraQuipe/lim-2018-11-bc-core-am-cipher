c8779969706e6b7b73b5c5b5dca44fdedcc1178c
Tu-username <tucorreo@gmail.com>
2018-12-04 21:17:24 -0500

    comentarios

--- src/cipher.js
+++ src/cipher.js
@@ -2,14 +2,14 @@ window.cipher = {
   encode:(offset,text) => {
   
   let  result = "";
-  for (let i= 0; i < text.length; i++){
-    const textMayuscula = text.toUpperCase().charCodeAt(i);
-    if (textMayuscula === 32) {
+  for (let i= 0; i < text.length; i++){                      //Recorriendo el texto ingresado para encode
+    const textMayuscula = text.toUpperCase().charCodeAt(i);  //Extrayendo el código Ascii y convirtiendo a MAYUSCULAS
+    if (textMayuscula === 32) {                              //Condición para los ESPACIOS de encode
     result+=  " ";} 
     else {
-      result = result + String.fromCharCode((textMayuscula+65+(offset%26))%26+65);}
+      result = result + String.fromCharCode((textMayuscula+65+(offset%26))%26+65);} //formula asscii
     }   
-  return result;  
+  return result;                                              //retorna el resultado encode
   },
 
   decode:(offset,text) => {
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const start=document.getElementById("btnempecemos");
+const start=document.getElementById("btnempecemos");//EVENTO PARA entrar a la 2 pagina
 start.addEventListener("click",()=>{
   document.getElementById ("contenedor1ero").style.display="none";
   document.getElementById ("contenedor2do").style.display="block";
 })
 
-document.getElementById("btncifrado").addEventListener("click", () =>{
+document.getElementById("btncifrado").addEventListener("click", () =>{ //eventocifrar
 const text = document.getElementById("cajadetexto").value;
-let offset = parseInt(document.getElementById("numerodecifrado").value);
+let offset = parseInt(document.getElementById("numerodecifrado").value); //devuelve un valor entero
 let result = window.cipher.encode(offset,text);
-let code =document.getElementById("textocifrado");
+let code = document.getElementById("textocifrado");
 code.value = result
 });
 
-document.getElementById("btndecifrado").addEventListener("click", () =>{
+document.getElementById("btndecifrado").addEventListener("click", () =>{ //eventodescifrar
 const text = document.getElementById("cajadetexto").value;
 let offset = parseInt(document.getElementById("numerodecifrado").value);
 let result = window.cipher.decode(offset,text);
